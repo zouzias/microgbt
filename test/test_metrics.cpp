49ce78d05b794fb58d636a483bba6b071958fe1a
Anastasios Zouzias <anastasios@sqooba.io>
2019-06-30 19:30:23 +0300

    [travis] add more tests

--- test/test_metrics.cpp
+++ test/test_metrics.cpp
@@ -8,6 +8,18 @@ TEST(microgbt, LogLoss)
     ASSERT_NEAR(logloss.logit(1.0), 1 / ( 1 + exp(-1.0)), 1.0e-11);
 }
 
+TEST(microgbt, LogLossClipUpper)
+{
+    microgbt::LogLoss logloss;
+    ASSERT_NEAR(logloss.clip(1.0), 1, 1.0e-7);
+}
+
+TEST(microgbt, LogLossClipLower)
+{
+    microgbt::LogLoss logloss;
+    ASSERT_NEAR(logloss.clip(0.0), 0, 1.0e-7);
+}
+
 TEST(microgbt, RMSE)
 {
     microgbt::RMSE rmse;
