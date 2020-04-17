de62d392a36779bcd3a422047a0d5261f9b8a3e5
Anastasios Zouzias <anastasios@sqooba.io>
2020-04-17 19:24:02 +0200

    fix python package

--- dev-scripts/runInstallPyPackage.sh
+++ dev-scripts/runInstallPyPackage.sh
@@ -1,29 +1,9 @@
 #!/usr/bin/env bash
 
-# Run make and copy shared library to examples
-
-pushd build || exit
-make
-# Copy .so library to examples for Python testing
-cp lib/*.so ../python-package/
-
-ls -lah lib/
-popd
-
-
 python3 -m pip install --user --upgrade pip
 python3 -m pip uninstall -y microgbtpy
-python3 -m pip install --user -U sklearn pandas wheel auditwheel
+python3 -m pip install --user -U sklearn pandas
 
-
-pushd python-package/
-echo "Building wheel..."
-rm -rf dist/
-python3 setup.py bdist_wheel
-
-echo "Repairing wheel by injecting shared library file"
-auditwheel repair dist/microgbtpy*.whl
-
-echo "Installing wheel..."
-pip3 install dist/microgbtpy*.whl -v
+echo "Installing package..."
+pip3 install .
 popd
