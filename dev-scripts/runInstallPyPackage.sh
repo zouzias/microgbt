5b2ada8e67012b441b000cc4f2a94f1157ad6f7b
Anastasios Zouzias <zouzias@users.noreply.github.com>
2024-11-17 19:16:29 +0100

    (pypackage) fix typo

--- dev-scripts/runInstallPyPackage.sh
+++ dev-scripts/runInstallPyPackage.sh
@@ -2,7 +2,7 @@
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip uninstall -y microgbtpy
-python3 -m pip install --user -U sklearn pandas numpu
+python3 -m pip install --user -U sklearn pandas numpy
 
 echo "Installing package..."
 pip3 install .
