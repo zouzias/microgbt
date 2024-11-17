6ffbe6139bbc4a9b6b16b282e554c076dec3e15a
Anastasios Zouzias <zouzias@users.noreply.github.com>
2024-11-17 09:55:12 +0100

    Update runInstallPyPackage.sh
    (pyreqs) add numpy

--- dev-scripts/runInstallPyPackage.sh
+++ dev-scripts/runInstallPyPackage.sh
@@ -2,7 +2,7 @@
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip uninstall -y microgbtpy
-python3 -m pip install --user -U sklearn pandas
+python3 -m pip install --user -U sklearn pandas numpu
 
 echo "Installing package..."
 pip3 install .
